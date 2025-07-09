926bdc990249570251e19e387015caeafa15899a
Samuel Ahou <87875158+SamuelAhou@users.noreply.github.com>
2025-07-09 09:56:37 +0200

    multiple pairs testing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from scripts.utils import *
 from scripts.strategies.PairsTrading import PairsTrading
 from scripts.strategies.SMA import SMAStrategy
 
+
 def get_polygon_data(ticker, start_date, end_date, api_key):
     client = RESTClient(api_key)
 
@@ -34,37 +35,44 @@ def get_polygon_data(ticker, start_date, end_date, api_key):
     return df
 
 if __name__ == '__main__':
-    API_KEY = "9NWEVqR5pPtWmzrq3NUI03lPU69iUntw"  # <-- Replace this with your actual Polygon API key
-    tickers = ['AAPL', 'GOOG']
-
-    dfs = []
-    for ticker in tickers:
-        df = get_polygon_data(ticker, "2013-01-01", "2025-01-01", API_KEY)
-
-        df.columns = pd.MultiIndex.from_product([[col.title() for col in df.columns], [ticker]])
-        dfs.append(df)
-
-    data = pd.concat(dfs, axis=1).sort_index()
-
-    # Strategy parameters
-    params_pairs = {
-        'entry_threshold': 2.0,
-        'exit_threshold': 0.5,
-        'order_size': 100.0,
-        'spread_type': 'zscore',  # Options: 'zscore', 'ratio', 'log-difference', 'kalman'
-        'zscore_window': 20,  # Only used if spread_type is 'z
-        'kalman_Q': 0.1,
-        'kalman_R': 0.1,
-        'kalman_window': 20
-    }
-
-    #data = yf.download(['PEP', 'KO'], start='2010-01-01', end='2020-01-01', progress=False)
-
-    pairs_strategy = PairsTrading('PairsTrading(AAPL-GOOG)-Kalman2', data, params_pairs, 1000.0)
-
-    pairs_strategy.run()
-    pairs_strategy.plot('/Users/Samuel/Documents/Projects/Algorithmic-Trading/results/PairsTrading')
+    API_KEY = ""  # <-- Replace this with your actual Polygon API key
     
-    print("Pairs Trading Strategy Results:")
-    for key, value in pairs_strategy.metrics.items():
-        print(f"{key}: {value}")
+    pairs = [['AAPL', 'GOOG'], ['PEP', 'KO'], ['MSFT', 'AMZN']]
+
+    for pair in pairs:
+
+        tickers = [pair[0], pair[1]]
+
+        dfs = []
+
+        for ticker in tickers:
+            df = get_polygon_data(ticker, "2023-01-01", "2025-01-01", API_KEY)
+
+            df.columns = pd.MultiIndex.from_product([[col.title() for col in df.columns], [ticker]])
+            dfs.append(df)
+
+        data = pd.concat(dfs, axis=1).sort_index()
+
+        # Strategy parameters
+        params_pairs = {
+            'entry_threshold': 2.0,
+            'exit_threshold': 0.5,
+            'order_size': 100.0,
+            'spread_type': 'zscore',  # Options: 'zscore', 'ratio', 'log-difference', 'kalman'
+            'zscore_window': 20,  # Only used if spread_type is 'z
+            'kalman_Q': 0.1,
+            'kalman_R': 0.1,
+            'kalman_window': 20
+        }
+
+        #data = yf.download(['PEP', 'KO'], start='2010-01-01', end='2020-01-01', progress=False)
+
+        strategy_name = '(' + '-'.join(pair) + ')' + '-' + params_pairs['spread_type']
+        pairs_strategy = PairsTrading(strategy_name, data, params_pairs, 1000.0)
+
+        pairs_strategy.run()
+        pairs_strategy.plot('results/PairsTrading')
+    
+        print("Pairs Trading Strategy Results:")
+        for key, value in pairs_strategy.metrics.items():
+            print(f"{key}: {value}")
